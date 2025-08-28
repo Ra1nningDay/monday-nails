740cc9fab8497214eaf98985233a2558b2254336
Ra1nningDay <tkhamkawe@gmail.com>
2025-08-29 03:37:20 +0700

    feat: add mascot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Reviews from "@/components/reviews";
 import FAQ from "@/components/faq";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
+import Mascot from "@/components/mascot";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Benefits />
       <Services />
       <Reviews />
+      <Mascot />
       <FAQ />
       <Contact />
       <Footer />
